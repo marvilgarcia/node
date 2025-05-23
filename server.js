14d5a41fb188390f23df984bc4b3ae78a96b7886
Marvil Garcia <marvigarcia015@gmail.com>
2025-05-22 22:02:37 -0400

    Initial project setup

--- server.js
+++ server.js
@@ -6,14 +6,28 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+
+View Engine and Templates
  * Routes
  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
 app.use(static)
+
+app.use(require)
+// Route for Home Page
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" });
+});
 
 /* ***********************
  * Local Server Information
@@ -28,3 +42,4 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
